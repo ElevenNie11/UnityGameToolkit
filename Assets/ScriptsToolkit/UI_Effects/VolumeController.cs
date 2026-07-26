eccb2be928db8266c2e62e787e259d74c5785466
Eleven <nieshiyi20060927@qq.com>
2026-07-27 00:17:59 +0800

    UI-音量滑动条

--- Assets/ScriptsToolkit/UI_Effects/VolumeController.cs
+++ Assets/ScriptsToolkit/UI_Effects/VolumeController.cs
@@ -7,24 +7,27 @@ public class VolumeController : MonoBehaviour
 {
     [Header("UI组件")]
     public Slider volumeSlider; //在Inspector中拖入VolumeSlider，脚本通过它读写滑条的值
-    private const string VOLUME_KEY = "MasterVolume";  ////PlayerPrefs键名:存档用的键名,用于PlayerPrefs的读写
+    private const string VOLUME_KEY = "MasterVolume";  //PlayerPrefs键名:存档用的键名,用于PlayerPrefs的读写
     void Start()
     {
         float savedVolume = PlayerPrefs.GetFloat(VOLUME_KEY, 0.75f); //从本地存档读取上次音量，如果没有存档就默认0.75
         volumeSlider.value = savedVolume;                            //把滑条的位置同步到存档的值
         ApplyVolume(savedVolume);                                    //立即应用音量，确保游戏启动时音量正确
-        volumeSlider.onValueChanged.AddListener(VolumeChanged);      //注册监听——滑条一变动就自动调用OnVolumeChanged
+        volumeSlider.onValueChanged.AddListener(VolumeChanged);      //注册监听——滑条一变动就自动调用VolumeChanged(监听函数)
     }
     //函数1:滑条变动时触发:每次用户拖动滑条，Unity自动传入当前值value
-    void VolumeChanged(float value)
+    public void VolumeChanged(float value)
     {
         ApplyVolume(value);
-        PlayerPrefs.SetFloat(VOLUME_KEY, volumeSlider.value);       ////保存设置 下次启动游戏仍然生效
+        PlayerPrefs.SetFloat(VOLUME_KEY, volumeSlider.value);        //保存设置 下次启动游戏仍然生效
     }
     //函数2：实际应用音量
-    void ApplyVolume(float value)
+    public void ApplyVolume(float value)
     {
         //AudioListener是Unity全局音频监听器
         AudioListener.volume = value;
     }
 }
+
+// PlayerPrefs 是Unity内置控制的轻量级存档系统（键值存储）：只支持3种数据类型：int  float  string
+// 其实也可以不要[ volumeSlider.onValueChanged.AddListener(VolumeChanged); ]这行代码：直接在Inspector里绑定函数VolumeChanged即可
