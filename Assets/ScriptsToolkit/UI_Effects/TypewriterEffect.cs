8d30bb745cbb194bc7e40f553231b64c316d37cc
Eleven <nieshiyi20060927@qq.com>
2026-07-26 22:47:15 +0800

    打字机效果：TMP_Text核心组件搭配协程

--- Assets/ScriptsToolkit/UI_Effects/TypewriterEffect.cs
+++ Assets/ScriptsToolkit/UI_Effects/TypewriterEffect.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+//一行数据的内容：文本格式为：说话人(speaker)|说话内容(content) 
+public class DialogueLine
+{
+    public string speaker;
+    public string content;
+    public DialogueLine(string speaker, string content)
+    {
+        this.speaker = speaker;
+        this.content = content;
+    }
+}
 
 public class TypewriterEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("文字TMPro")]
+    public TMP_Text contentText;
+    [Header("打字机效果的速度：数值越小越快")]
+    public float typeSpeed = 0.03f;
+    //协程 IEnumerator
+    private IEnumerator TypeOneLine(DialogueLine line)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        contentText.text = " ";          //先清空文本框
+        foreach (char c in line.content)
+        {
+            contentText.text += c;
+            yield return new WaitForSeconds(typeSpeed);   //等待一段时间再显示下一个字符
+        }
     }
 }
+
+/*-------------*/
+//TMP_Text 是 TextMeshPro 系统中的核心抽象文本组件，在Unity里左UI文字工具包的时候，应该优先围绕它设计展开
+//TMP_Text 的关键API：
+//1. [设置文字]: contentText.text = "你好";
+//2. [获取文字]：string value = contentText.text;
+//3. [清空文字]：contentText.text = "";
