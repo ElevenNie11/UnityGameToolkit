f8d69e224a010a94fbe46fcd51a035645980aeaa
Eleven <nieshiyi20060927@qq.com>
2026-08-14 11:18:10 +0800

    道具数据类

---
+++ Assets/ScriptsToolkit/InventorySystem/InventoryItemData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+[System.Serializable]
+public class InventoryItemData
+{
+    public int itemId;          //道具ID
+    public string itemName;     //道具名
+    public Sprite icon;         //图标
+    public int count;           //数量
+    public string description;  //道具详情描述
+}
