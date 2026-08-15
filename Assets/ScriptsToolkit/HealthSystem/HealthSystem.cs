8dd7f5c96f2f425af57e31ba295af14211f5dbef
Eleven <nieshiyi20060927@qq.com>
2026-08-15 17:54:09 +0800

    血条UI

---
+++ Assets/ScriptsToolkit/HealthSystem/HealthSystem.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class HealthSystem : MonoBehaviour
+{
+    public static HealthSystem Instance;
+    [Header("血量数据")]
+    public int maxHealth = 100;
+    public int currentHealth = 10;
+
+    [Header("血量UI Slider引用")]
+    public Slider healthSlider;
+    public TextMeshProUGUI healthText;   //显示百分比
+
+    void Awake()
+    {
+        Instance = this;
+    }
+     void Start()
+    {
+        UpdateHealthBar();
+    }
+
+    //更新血量UI
+    public void UpdateHealthBar()
+    {
+       float percent = (float)currentHealth / maxHealth;
+       healthSlider.value = percent * 100;
+       if(healthText != null)
+       {
+           //显示百分比：RoudToInt表示四舍五入取整
+           healthText.text = Mathf.RoundToInt(percent * 100) + "%";
+       }
+    }
+
+    //1. 回血:percent 百分比（传10就是+10%）
+    public void Heal(int percent)
+    {
+        int healAmount = Mathf.RoundToInt(maxHealth * percent / 100f);
+        currentHealth += healAmount;
+        UpdateHealthBar();
+    }
+    //2. 掉血
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        UpdateHealthBar();
+    }
+}
