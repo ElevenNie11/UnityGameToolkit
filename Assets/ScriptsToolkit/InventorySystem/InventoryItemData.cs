13a0ba9f2e1fdfab5c8531abb49a3f25b16eb731
Eleven <nieshiyi20060927@qq.com>
2026-08-16 13:20:45 +0800

    扩展道具数据以及商品分类枚举

--- Assets/ScriptsToolkit/InventorySystem/InventoryItemData.cs
+++ Assets/ScriptsToolkit/InventorySystem/InventoryItemData.cs
@@ -1,4 +1,9 @@
 using UnityEngine;
+public enum ItemCategory
+{
+    Food,   //食物
+    Weapon  //武器
+}
 [System.Serializable]
 public class InventoryItemData
 {
@@ -8,4 +13,6 @@ public class InventoryItemData
     public int count;           //数量
     public string description;  //道具详情描述
     public int healPercent = 10;     //回血百分比（0~100）默认10%
+    public int price = 0;        //商品价格
+    public ItemCategory category; //商品分类：食物和武器
 }
