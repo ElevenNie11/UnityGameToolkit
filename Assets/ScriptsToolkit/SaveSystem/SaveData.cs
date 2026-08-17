ab7e7c02063f30cb31f094c4c259fbc590240f48
Eleven <nieshiyi20060927@qq.com>
2026-08-17 19:19:02 +0800

    存档纯数据类

--- Assets/ScriptsToolkit/SaveSystem/SaveData.cs
+++ Assets/ScriptsToolkit/SaveSystem/SaveData.cs
@@ -1,6 +1,10 @@
+//纯数据类
 using UnityEngine;
-
+[System.Serializable]
 public class SaveData : MonoBehaviour
 {
-    
+    public int currentHealth;  //血量
+    public int coins;          //金币
+    public int questIndex;     //任务索引
+    public bool questCompleted;//任务是否完成
 }
