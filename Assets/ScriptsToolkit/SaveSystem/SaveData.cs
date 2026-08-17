b0ddf8a6cc05d37cb84a3b6a906fcb339cf5ad8c
Eleven <nieshiyi20060927@qq.com>
2026-08-17 20:05:50 +0800

    修正报错

--- Assets/ScriptsToolkit/SaveSystem/SaveData.cs
+++ Assets/ScriptsToolkit/SaveSystem/SaveData.cs
@@ -1,7 +1,6 @@
-//纯数据类
-using UnityEngine;
+//纯数据类：不能继承MonoBehaviour, 否则会Json反序列化报错
 [System.Serializable]
-public class SaveData : MonoBehaviour
+public class SaveData
 {
     public int currentHealth;  //血量
     public int coins;          //金币
